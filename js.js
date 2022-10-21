7d8e04bf9c52a8eb99662e5048c524e26c2e4081
Coltensiemer <colten50@hotmail.com>
2022-10-20 21:53:54 -0400

    update

--- js.js
+++ js.js
@@ -1,10 +1,26 @@
-const tabs = document.querySelectorAll('data-tag-target'); 
+const tabs = document.querySelectorAll('[data-tab-target'); 
+const tabContents = document.querySelectorAll('[data-tab-content'); 
+
+const body = document.body; 
+
+const home = document.querySelector('.Home-Menu'); 
 
 
-tabs.forEach(tab => {
+
+tabs.forEach(tab => { 
     tab.addEventListener('click', () => { 
-        const target = document.querySelector(tab.dataset.tabTarget); 
-    target.classList.add('active');
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+        tabContent.classList.remove('active')
     })
-    
-});
+    tabs.forEach(tab => { 
+        tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+    })
+})
+
+home.addEventListener('click', () => { 
+    body.style.background.blue; 
+})
